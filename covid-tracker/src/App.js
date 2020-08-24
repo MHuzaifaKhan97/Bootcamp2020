e5bd84ea6261e6f4728fb3190df07e13a0f17ad4
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2020-08-23 17:24:05 -0700

    Project 02: Covid Tracker App

---
+++ covid-tracker/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import InfoPanel from './components/InfoPanel';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <InfoPanel />
+    </div>
+  );
+}
+
+export default App;
